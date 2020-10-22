59a8d83c7cc249cd29725aa52efbe225cf83985a
danielhorizon <taesoodaniellee@gmail.com>
2020-10-22 13:27:55 -0400

    adding in mnist

---
+++ multiclass_src/mnist.py
@@ -0,0 +1,138 @@
+import click 
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+def show_image(loader):
+    examples = enumerate(loader)
+    batch_idx, (example_data, example_targets) = next(examples)
+
+    fig = plt.figure()
+    for i in range(6):
+        plt.subplot(2, 3, i+1)
+        plt.tight_layout()
+        plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+        plt.title("Ground Truth: {}".format(example_targets[i]))
+        plt.xticks([])
+        plt.yticks([])
+        print(fig)
+
+
+def load_data(show=False): 
+    batch_size_train = 64
+    batch_size_test = 1000
+
+    transform = transform = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(
+            (0.1307,), (0.3081,))
+    ])
+
+    train_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST(root='../data', train=True, download=True,
+            transform=transform), batch_size=batch_size_train, shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST(root='../data', train=False, download=True,
+            transform=transform), batch_size=batch_size_test, shuffle=True)
+    
+    if show: 
+        show_image(test_loader) 
+
+    return train_loader, test_loader
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+
+
+def train_mnist(loss_metric=None, epochs=None):
+    
+    train_loader, test_loader = load_data(show=False)
+
+    train_losses = []
+    train_counter = []
+    test_losses = []
+
+    log_interval = 10
+    torch.manual_seed(1)
+
+    model = Net()
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+    if loss_metric == "ce":
+        criterion = nn.CrossEntropyLoss()
+    else:
+        criterion = nn.CrossEntropyLoss()
+
+    for epoch in range(epochs): 
+        # TRAIN MODEL 
+        model.train()
+        for batch_idx, (data, target) in enumerate(train_loader):
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+
+
+            if batch_idx % log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), loss.item()))
+                train_losses.append(loss.item())
+                train_counter.append(
+                    (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
+
+                # saving model 
+                # torch.save(model.state_dict(), '/results/model.pth')
+                # torch.save(optimizer.state_dict(), '/results/optimizer.pth')
+
+
+        # TEST MODEL 
+        model.eval()
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for data, target in test_loader:
+                output = model(data)
+                test_loss += F.nll_loss(output, target, size_average=False).item()
+                pred = output.data.max(1, keepdim=True)[1]
+                correct += pred.eq(target.data.view_as(pred)).sum()
+        test_loss /= len(test_loader.dataset)
+        test_losses.append(test_loss)
+        print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
+
+
+@click.command()
+@click.option("--loss", required=True)
+@click.option("--epochs", required=True)
+def run(loss, epochs):
+    train_mnist(loss_metric='ce', epochs=int(epochs))
+
+def main():
+    run()
+
+
+if __name__ == '__main__':
+    main()
